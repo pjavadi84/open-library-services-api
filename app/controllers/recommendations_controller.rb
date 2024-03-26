3abe39067083747b2139e4a5482a93330784747a
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-03-25 21:52:07 -0700

    configured recommendations  routes

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,2 +1,8 @@
 class RecommendationsController < ApplicationController
+    def index
+        query = params[:query] || 'Ruby'
+        books = OpenLibraryService.search_books(query)
+        render json: books
+      end
+      
 end
