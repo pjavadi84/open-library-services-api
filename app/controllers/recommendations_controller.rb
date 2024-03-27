5136b88c77f7e3492dbf932a382a14f4488d252f
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-03-27 00:50:00 -0700

    modified controller to send request to sidekiq

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,8 +1,8 @@
 class RecommendationsController < ApplicationController
     def index
-        query = params[:query] || 'Ruby'
-        books = OpenLibraryService.search_books(query)
-        render json: books
-      end
-      
+        # Trigger the background job with a default query or based on user input
+      FetchBookDataJob.perform_later('Ruby')
+        # Immediately respond to the user, don't wait for the job to finish
+      render json: { message: 'Fetching book data, please check back soon!' }
+    end
 end
